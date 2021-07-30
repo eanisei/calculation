4307de0e192f7ad3ea97a8077fd7c37fa94b3c8b
Eugen Anisei <eanisei@gmail.com>
2021-07-30 16:42:24 +0200

    changed index file due to wrong header

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-let money, 
- income, 
- addExpenses, 
- deposit, 
- mission, 
- period;
+let money = 20000; 
+let income = 'freelance'; 
+let addExpenses = 'food, rent, eating out';
+let deposit = false;
+let mission = 1000000; 
+let period = 8;
 
 
  alert('Step 1 made');
 
  console.log('Congratulations');
+ console.log(typeof money);
+ console.log(typeof income);
+ console.log(typeof deposit);
+
+ console.log(addExpenses.length);
+
