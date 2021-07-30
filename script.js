25e4750c1960abe2245860ddd3bb1fa68de7a5cd
Eugen Anisei <eanisei@gmail.com>
2021-07-30 17:07:45 +0200

    ready

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-let money, 
- income, 
- addExpenses, 
- deposit, 
- mission, 
- period;
-
+let money = 20000; 
+let income = 'freelance'; 
+let addExpenses = 'food, rent, having fun'; 
+let deposit = false; 
+let mission = 1000000; 
+let period = 8;
+let budgetDay = money/30;
 
  alert('Step 1 made');
 
  console.log('Congratulations');
+
+
+ console.log(typeof money);
+ console.log(typeof income);
+ console.log(typeof deposit);
+
+ console.log(addExpenses.length);
+
+ console.log('Период равен ' + period + ' месяцев');
+ console.log('Цель - заработать ' + mission + ' крон');
+
+
+ addExpenses.toLowerCase();
+ console.log(addExpenses.split(', '));
+
+ console.log(budgetDay);
